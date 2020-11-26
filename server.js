26b71d2c8518773fcdfb7333bbd7b9e675d22f61
Alexandra Nadova <alexandranadova@gmail.com>
2020-11-26 12:46:38 +0100

    user functionality

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello world!" });
 });
 
+require("./app/routes/user.js")(app);
+
 // set port, listen for requests
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
