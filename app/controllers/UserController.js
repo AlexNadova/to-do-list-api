82a22d9f9cdc85fa50c73f07c822ef2f85d4d9ca
Alexandra Nadova <alexandranadova@gmail.com>
2020-11-28 16:07:08 +0100

    update, prepare for auth

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,53 +1,24 @@
 const User = require("../models/UserModel.js");
 
-// Create and Save a new User
-exports.create = (req, res) => {
-  // Validate request
-  if (!req.body) {
-    res.status(400).send({
-      message: "Content can not be empty!",
-    });
-  }
-
-  // Create a User
-  const user = new User({
-    email: req.body.email,
-    name: req.body.name,
-    password: req.body.password,
-  });
-
-  // Save User in the database
-  User.create(user, (err, data) => {
-    if (err)
-      res.status(500).send({
-        message: err.message || "Some error occurred while creating the User.",
-      });
-    else res.send(data);
-  });
+exports.allAccess = (req, res) => {
+  res.status(200).send("Public Content.");
 };
 
-// Retrieve all Users from the database.
-exports.findAll = (req, res) => {
-  User.getAll((err, data) => {
-    if (err)
-      res.status(500).send({
-        message: err.message || "Some error occurred while retrieving users.",
-      });
-    else res.send(data);
-  });
+exports.userBoard = (req, res) => {
+  res.status(200).send("User Content.");
 };
 
 // Find a single User with a userId
 exports.findOne = (req, res) => {
-  User.findById(req.params.userId, (err, data) => {
+  User.findById(req.userId, (err, data) => {
     if (err) {
       if (err.kind === "not_found") {
         res.status(404).send({
-          message: `Not found User with id ${req.params.userId}.`,
+          message: `Not found User with id ${req.userId}.`,
         });
       } else {
         res.status(500).send({
-          message: "Error retrieving User with id " + req.params.userId,
+          message: "Error retrieving User with id " + req.userId,
         });
       }
     } else res.send(data);
--- app/models/db.js
+++ app/models/db.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql");
-const dbConfig = require("../../config/db.config.js");
+const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
 });
 
 // open the MySQL connection
-connection.connect(error => {
+connection.connect((error) => {
   if (error) throw error;
   console.log("Successfully connected to the database.");
 });
